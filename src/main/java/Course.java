9a043f4dbff5f8da5a1ac8780d490edfcee873ec
keetdaly <keithdaly14@hotmail.com>
2018-10-11 13:10:48 +0100

    Added code for Course class

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,60 @@
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+
 public class Course {
+
+    private String name;
+    private ArrayList<Module> modules;
+    private ArrayList<Student> students;
+    private DateTime startDate;
+    private DateTime endDate;
+
+    public Course(String name, ArrayList modules, ArrayList students, DateTime startDate, DateTime endDate) {
+        this.setName(name);
+        this.setModules(modules);
+        this.setStudents(students);
+        this.setStartDate(startDate);
+        this.setEndDate(endDate);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(ArrayList<Module> modules) {
+        this.modules = modules;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public DateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(DateTime endDate) {
+        this.endDate = endDate;
+    }
 }
