eb50bb44dbeeb901c304a78cb80bd203abd59854
keetdaly <keithdaly14@hotmail.com>
2018-10-11 13:01:57 +0100

    Wrote code for Module class

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class Module {
+
+    private String name;
+    private String id;
+    private ArrayList<Student> students;
+    private ArrayList<Course> associatedCourses;
+
+    public Module(String name, String id, ArrayList students, ArrayList associatedCourses) {
+        this.setName(name);
+        this.setId(id);
+        this.setStudents(students);
+        this.setAssociatedCourses(associatedCourses);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Course> getAssociatedCourses() {
+        return associatedCourses;
+    }
+
+    public void setAssociatedCourses(ArrayList<Course> associatedCourses) {
+        this.associatedCourses = associatedCourses;
+    }
 }
